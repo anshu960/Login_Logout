ab5f86acf4dc25d2ea619b15b67c36c4c0afcc04
Anshu <anshukumarsingh448@gmail.com>
2022-06-22 21:23:19 +0530

    inital

--- app/src/main/java/com/example/login_logout/MainActivity.kt
+++ app/src/main/java/com/example/login_logout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.login_logout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.login_logout.databinding.ActivityMainBinding
@@ -14,10 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loginBtn.setOnClickListener {
-
+           startActivity(Intent(this, LoginActivity::class.java))
         }
         binding.skipBtn.setOnClickListener {
-
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
