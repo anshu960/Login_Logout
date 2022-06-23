908d43a16f1c7aea6fcdbdceaca9ce774e73fd34
Anshu <anshukumarsingh448@gmail.com>
2022-06-23 17:40:35 +0530

    inital

--- app/src/main/java/com/example/login_logout/RegisterActivity.kt
+++ app/src/main/java/com/example/login_logout/RegisterActivity.kt
@@ -1,11 +1,115 @@
 package com.example.login_logout
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.login_logout.databinding.ActivityMainBinding
+import com.example.login_logout.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //init progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //handle back button
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
+
+        //handler click begin
+        binding.registerBtn.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private var name = ""
+    private var email = ""
+    private var mobile = ""
+    private var password = ""
+
+    private fun validateData() {
+        name = binding.nameEt.text.toString().trim()
+        email = binding.emailEt.text.toString().trim()
+        mobile = binding.mobileEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        if (name.isEmpty()){
+            Toast.makeText(this, "Enter your name...", Toast.LENGTH_SHORT).show()
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+            Toast.makeText(this, "Invalid Email Pattern...", Toast.LENGTH_SHORT).show()
+        }
+        else if (mobile.isEmpty()){
+            Toast.makeText(this, "Enter your mobile...", Toast.LENGTH_SHORT).show()
+        }
+        else if (password.isEmpty()){
+            Toast.makeText(this, "Enter Your Password...", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            createUserAccount()
+        }
+    }
+
+    private fun createUserAccount() {
+        progressDialog.setMessage("Creating Account...")
+        progressDialog.show()
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                updateUserInfo()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed creating account due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun updateUserInfo() {
+        progressDialog.setMessage("Saving user info...")
+
+        val timestamp = System.currentTimeMillis()
+
+        val uid = firebaseAuth.uid
+        val hashMap: HashMap<String, Any?> = HashMap()
+        hashMap["uid"] = uid
+        hashMap["email"] = email
+        hashMap["name"] = name
+        hashMap["mobile"] = mobile
+        hashMap["profileImage"] = ""
+        hashMap["userType"] = "user"
+        hashMap["timestamp"] = timestamp
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(uid!!)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+               progressDialog.dismiss()
+               Toast.makeText(this, "Account created.", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@RegisterActivity, DashboardActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed creating account due to ${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
     }
 }
