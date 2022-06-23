ee1fb58ce33d21063f0999f882c6a7775795b08d
Anshu <anshukumarsingh448@gmail.com>
2022-06-23 20:08:53 +0530

    inital

--- app/src/main/java/com/example/login_logout/LoginActivity.kt
+++ app/src/main/java/com/example/login_logout/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.example.login_logout
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login_logout.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    //viewbinding
+    private lateinit var binding: ActivityLoginBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //init progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //handle click
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.loginBtn.setOnClickListener {
+            validateData()
+        }
+    }
+    private var email = ""
+    private var password = ""
+    private fun validateData() {
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
     }
 }
